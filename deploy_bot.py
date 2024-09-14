b92f251eb0276c0b070c823c1fcf1b92309509c9
Sasoed <119231767+Sasoed@users.noreply.github.com>
2024-09-14 19:37:00 +0300

    Update deploy_bot.py

--- deploy_bot.py
+++ deploy_bot.py
@@ -1,22 +1,30 @@
 import json
 import os
 import subprocess
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 
+def check_or_create_api_txt():
+    if not os.path.exists("api.txt"):
+        with open("api.txt", "w") as file:
+            file.write("YOUR_API_KEY")
+        print("api.txt created.")
+
 def read_token():
-    with open('/home/seva_romanovsky/bot/Deploy_Bot/API.txt', 'r') as file:
-        token = file.read().strip()
-    return token
+    with open('api.txt', 'r') as file:
+        return file.read().strip()
 
 def read_allowed_users():
-    with open('/home/seva_romanovsky/bot/Deploy_Bot/allowed_users.json', 'r') as file:
-        data = json.load(file)
-    return set(data['allowed_users'])
+    try:
+        with open('allowed_users.json', 'r') as file:
+            data = json.load(file)
+            return set(data['allowed_users'])
+    except FileNotFoundError:
+        return set()
 
 def read_project_paths():
     try:
-        with open('/home/seva_romanovsky/bot/Deploy_Bot/project_paths.json', 'r') as file:
+        with open('project_paths.json', 'r') as file:
             return json.load(file)
     except FileNotFoundError:
         return {}
@@ -24,7 +32,7 @@ def read_project_paths():
 def save_project_path(repo_name, path):
     paths = read_project_paths()
     paths[repo_name] = path
-    with open('/home/seva_romanovsky/bot/Deploy_Bot/project_paths.json', 'w') as file:
+    with open('project_paths.json', 'w') as file:
         json.dump(paths, file)
 
 TOKEN = read_token()
@@ -44,10 +52,8 @@ async def deploy(update: Update, context: ContextTypes.DEFAULT_TYPE):
     repo_name = github_link.split('/')[-1].replace('.git', '')
     project_path = f'/home/seva_romanovsky/bot/{repo_name}'
 
-    # Сохраняем путь проекта
     save_project_path(repo_name, project_path)
 
-    # Клонирование репозитория и настройка сервиса
     commands = [
         f"git clone {github_link} {project_path}",
         f"echo '[Unit]\\nDescription=Python Application\\nAfter=network.target\\n\\n[Service]\\nType=simple\\nUser=seva_romanovsky\\nWorkingDirectory={project_path}\\nExecStart=/home/seva_romanovsky/env/bin/python3 {project_path}/{exec_file}\\n\\n[Install]\\nWantedBy=multi-user.target' > {project_path}/{repo_name}.service",
@@ -69,7 +75,6 @@ async def deploy(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def update_project(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
-
     repo_name = query.data.split('_')[1]
     project_paths = read_project_paths()
     project_path = project_paths.get(repo_name)
@@ -89,17 +94,21 @@ async def update_project(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if process.returncode != 0:
             await query.edit_message_text(f'Ошибка при выполнении команды {cmd}: {stderr.decode()}')
             return
+
         await query.edit_message_text(f'Результат выполнения {cmd}: {stdout.decode()}')
 
     await query.edit_message_text('Проект обновлен и перезапущен.')
 
 def main():
+    check_or_create_api_txt()
     app = Application.builder().token(TOKEN).build()
+
     deploy_handler = CommandHandler('deploy', deploy)
     update_handler = CallbackQueryHandler(update_project, pattern='^update_')
 
     app.add_handler(deploy_handler)
     app.add_handler(update_handler)
+
     app.run_polling()
 
 if __name__ == '__main__':
